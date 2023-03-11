649bee80efd6dfd9062cc1bb13122b979e41823b
IT22167996 <127421151+IT22167996@users.noreply.github.com>
2023-03-11 06:08:55 +0000

    preparing Tute03.c - Exercise 3

--- .ccls-cache/@home@runner@tutorial-01-IT22167996/Tute03.c
+++ .ccls-cache/@home@runner@tutorial-01-IT22167996/Tute03.c
@@ -11,9 +11,20 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
+// preprocessor directives
 #include <stdio.h>
+
+// main function
 int main() {
-  
+  // variables
+
+  // taking input for 'n'
+
+  // loop to keep the program running till it stops when needed
+
+  // displaying the output
+
+  // signifying successful program execution
   return 0;
 }
-
+// end of main function
