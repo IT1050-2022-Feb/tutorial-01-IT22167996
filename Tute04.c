03224f1b322013b5ab24c5ce09978a645bfa723f
IT22167996 <127421151+IT22167996@users.noreply.github.com>
2023-03-12 17:53:52 +0000

    completed Tute04.c - Exercise 4

--- Tute04.c
+++ Tute04.c
@@ -4,8 +4,15 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
+// preprocessor directives
 #include <stdio.h>
 
+// prototypes of the custom functions
+int minimum(int, int);
+int maximum(int, int);
+int multiply(int, int);
+
+// main function
 int main() {
    int no1, no2;
    printf("Enter a value for no 1 : ");
@@ -17,3 +24,30 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+// end of main function
+
+// custom function implementation for minimum
+int minimum(int num1, int num2) {
+  if (num1 > num2) {
+    return num2;
+  } else {
+    return num1;
+  }
+}
+// end of custom funtion
+
+// custom function implementation for maximum
+int maximum(int num1, int num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+// end of custom funtion
+
+// custom function implementation for multiply
+int multiply(int num1, int num2) {
+  return num1 * num2;
+}
+// end of custom funtion
